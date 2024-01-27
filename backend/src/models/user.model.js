9d253dab943d5f10b759e223a32303af14dc754b
nikhil kumar <nikhilkrsolanki@gmail.com>
2024-01-27 15:27:39 +0530

    created user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,76 @@
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('../db/config');
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        firstName: {
+            type: DataTypes.STRING(30),
+            allowNull: {
+                args: false,
+                msg: 'first name is required',
+            },
+        },
+        lastName: {
+            type: DataTypes.STRING(30),
+            allowNull: {
+                args: false,
+                msg: 'last name is required',
+            },
+        },
+        email: {
+            type: DataTypes.STRING(50),
+            allowNull: {
+                args: false,
+                msg: 'email is required',
+            },
+            unique: {
+                args: true,
+                msg: 'email already exists',
+            },
+            validate: {
+                isEmail: {
+                    args: true,
+                    msg: 'enter a valid email',
+                },
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: {
+                args: false,
+                msg: 'password is required',
+            },
+            validate: {
+                len: {
+                    args: [6, 12],
+                    msg: 'password must be in between 6 to 12 characters',
+                },
+            },
+        },
+    },
+    {
+        sequelize,
+        modelName: 'users',
+        timestamps: false,
+    }
+);
+
+(async function () {
+    try {
+        await User.sync({ alter: true });
+        console.log('The table for the User model was just (re)created!');
+    } catch (error) {
+        console.log('Unable to sync User model', error);
+    }
+})();
+
+module.exports = {
+    User
+}
